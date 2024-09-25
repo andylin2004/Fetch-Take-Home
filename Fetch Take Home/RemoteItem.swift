dbf2194f82cf26ca9f2da89f8afbba0f3d5f2a12
Andy Lin <andy.lin.2004@outlook.com>
2024-09-25 16:22:09 +0100

    added remoteitem struct with static function to pull the data

--- Fetch Take Home/RemoteItem.swift
+++ Fetch Take Home/RemoteItem.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct RemoteItem: Codable {
+    let id: Int
+    let listId: Int
+    let name: String?
+    
+    static func pullItems() async throws -> [RemoteItem] {
+        let url = URL(string: "https://fetch-hiring.s3.amazonaws.com/hiring.json")
+        if let url = url {
+            let (pulledData, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            return try decoder.decode([RemoteItem].self, from: pulledData)
+        } else {
+            throw URLError(.badURL)
+        }
+    }
+}
