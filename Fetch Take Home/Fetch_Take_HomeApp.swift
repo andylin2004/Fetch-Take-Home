8799f75e51aa9bf7b920b66550915f5cc265841b
Andy Lin <andy.lin.2004@outlook.com>
2024-09-25 16:13:32 +0100

    Initial Commit

---
+++ Fetch Take Home/Fetch_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_HomeApp.swift
+//  Fetch Take Home
+//
+//  Created by Andy Lin on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
