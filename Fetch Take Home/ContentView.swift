5dbd9e916340d35a9fe2d163af1a511fa0f006ba
Andy Lin <andy.lin.2004@outlook.com>
2024-09-28 10:52:39 +0200

    improved state handling of error states

--- Fetch Take Home/ContentView.swift
+++ Fetch Take Home/ContentView.swift
@@ -7,27 +7,56 @@
 
 import SwiftUI
 
-// this view hosts the whole list, including the navigation bar. This list uses a subview for each section. This view also handles the inital task of getting all remote items
+// this view hosts the whole list, including the navigation bar. This list uses a subview for each section. This view also handles the initial task of getting all remote items
 struct ContentView: View {
-    @State var remoteItemsByListId: [Int : [RemoteItem]] = [:]
+    // this variable contains all the remote items
+    @State private var remoteItemsByListId: [Int : [RemoteItem]] = [:]
+    
+    // this variable indicates the view state (loaded, unloaded, error)
+    @State private var loadingStatus: LoadingStatus = .notLoadedYet
     
     var body: some View {
         NavigationStack {
-            List(remoteItemsByListId.keys.sorted(), id: \.self) { listId in
-                ListIDGroupView(listId: listId, remoteItemsByListId: $remoteItemsByListId)
+            Group {
+                switch loadingStatus {
+                case .loaded:
+                    List(remoteItemsByListId.keys.sorted(), id: \.self) { listId in
+                        ListIDGroupView(listId: listId, remoteItemsByListId: $remoteItemsByListId)
+                    }
+                    .listStyle(.sidebar)
+                    .refreshable {
+                        await loadItems()
+                    }
+                case .error(let error):
+                    ContentUnavailableView {
+                        Label("Unable to Load Items", systemImage: "exclamationmark.triangle")
+                    } description: {
+                        Text(error.localizedDescription)
+                    } actions: {
+                        Button {
+                            Task {
+                                loadingStatus = .notLoadedYet
+                                await loadItems()
+                            }
+                        } label: {
+                            Label("Try loading again?", systemImage: "arrow.clockwise")
+                        }
+                    }
+                case .notLoadedYet:
+                    ProgressView("LOADING")
+                        .font(.body.smallCaps())
+                }
             }
-            .listStyle(.sidebar)
             .navigationTitle("Remote Items")
         }
         .task {
-            if let result = try? await RemoteItem.pullItems() {
-                self.remoteItemsByListId = result.filterAllEmptyOrNullNames().groupByListId()
-            }
+            loadingStatus = .notLoadedYet
+            await loadItems()
         }
     }
     
     // this view is here because each section of a list has a disclosure header that allows the rest of the list to show up or disappear
-    struct ListIDGroupView: View {
+    private struct ListIDGroupView: View {
         var listId: Int
         
         @Binding var remoteItemsByListId: [Int : [RemoteItem]]
@@ -43,6 +72,23 @@ struct ContentView: View {
                 }
                 .headerProminence(.increased)
             }
+        }
+    }
+    
+    private enum LoadingStatus {
+        case notLoadedYet
+        case loaded
+        case error(Error)
+    }
+    
+    // this function will attempt to load in all remote items from the remote source, or set a view state variable to show an error screen
+    private func loadItems() async {
+        do {
+            let result = try await RemoteItem.pullItems()
+            self.remoteItemsByListId = result.filterAllEmptyOrNullNames().groupByListId()
+            loadingStatus = .loaded
+        } catch {
+            loadingStatus = .error(error)
         }
     }
 }
